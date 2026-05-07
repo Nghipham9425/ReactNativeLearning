8aef4939f359fb7d13a4663b37a5684575bd7765
nghip <23DH112274@st.huflit.edu.vn>
2026-05-08 00:00:42 +0700

    button and state

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { useState } from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+  const [name, setName] = useState<string>("")
+  const [count, setCount] = useState<number>(0)
+  const [test, setTest] = useState({
+    name: "nghipham",
+    age: 12,
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <View>
-        <Text style={styles.header}>HEader</Text>
-      </View>
+      <Text style={{ fontSize: 60, fontWeight: "600" }}>
+        {test.name} Hello World
+      </Text>
+      <Text style={{ fontSize: 60 }}>count = {count}</Text>
+      <Button title="rblox" onPress={() => setCount(count + 1)} />
     </View>
   )
 }
